9842a60b9e01ee53f13a45ed420100a49f8ae239
steve-gray <steve@eventualconsistency.net>
2018-01-28 09:17:12 +1000

    Code Coverage - Helper functions.

---
+++ helpers_test.go
@@ -0,0 +1,37 @@
+package eventsourcing
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestRetryOperation checks that the retry will attempt an operation multiple times.
+func TestRetryOperation(t *testing.T) {
+	count := 0
+
+	errOutcome := Retry(10, func() error {
+		count++
+		if count < 5 {
+			return NewConcurrencyFault("dummy-key", int64(count))
+		}
+
+		return nil
+	})
+
+	assert.Nil(t, errOutcome, "The retry should not return an error.")
+	assert.Equal(t, 5, count, "The count should be 5 at the end of the test.")
+}
+
+// TestRetryBailout checks that we won't keep trying forever.
+func TestRetryBailout(t *testing.T) {
+	count := 0
+
+	errOutcome := Retry(10, func() error {
+		count++
+		return NewConcurrencyFault("dummy-key", int64(count))
+	})
+
+	assert.NotNil(t, errOutcome, "The retry should return an error.")
+	assert.Equal(t, 10, count, "The count should be 10 at the end of the test.")
+}
