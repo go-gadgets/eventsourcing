e958ecd9cb7523ebc472cf8c936d3f2bfec97197
steve-gray <steve@eventualconsistency.net>
2018-02-04 18:43:30 +1000

    Kafka - cleanup test code, add benchmark.

--- distribution/kafka/pubsub_test.go
+++ distribution/kafka/pubsub_test.go
@@ -1,13 +1,18 @@
 package kafka
 
 import (
+	"fmt"
+
 	"testing"
 
 	"github.com/go-gadgets/eventsourcing"
+	uuid "github.com/satori/go.uuid"
 )
 
 const (
 	testDomain = "TestDomain"
+	testHost   = "localhost:9092"
+	testTopic  = "testing"
 )
 
 var testRegistry eventsourcing.EventRegistry
@@ -25,8 +30,7 @@ type TickEvent struct {
 // TestKafkaPublishing tests kafka-publishing is operating as expected
 // using the CreatePublisher() API.
 func TestKafkaPublishing(t *testing.T) {
-	clusterHosts := []string{"localhost:9092"}
-	testTopic := "testing"
+	clusterHosts := []string{testHost}
 
 	// Create a publisher
 	pub, errSetup := CreatePublisher(clusterHosts, testTopic, testRegistry)
@@ -39,5 +43,28 @@ func TestKafkaPublishing(t *testing.T) {
 	if errPublished != nil {
 		t.Error(errPublished)
 		return
+	}
+}
+
+// BenchmarkKafkaPublish tests how fast we can run the publisher
+func BenchmarkIndividualCommmits(b *testing.B) {
+	clusterHosts := []string{testHost}
+
+	// Create a publisher
+	pub, errSetup := CreatePublisher(clusterHosts, testTopic, testRegistry)
+	if errSetup != nil {
+		b.Error(errSetup)
+		return
+	}
+
+	testKey := fmt.Sprintf("%s", uuid.NewV4())
+	count := 0
+	for i := 0; i < b.N; i++ {
+		count++
+		errPublished := pub.Publish(testKey, int64(count), TickEvent{Numeral: 1234})
+		if errPublished != nil {
+			b.Error(errPublished)
+			return
+		}
 	}
 }
